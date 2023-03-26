f96fb3e6fea1db51a530cddff49a613cde733f90
donghyune <tjswk70000@lab2m.com>
2023-03-26 22:14:03 +0900

    wpqkfwha

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
+import Layout, { siteTitle } from '../components/Layout';
 import utilStyles from '../styles/utils.module.css';
-import { useState, useEffect } from 'react';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
-import Date from '../components/date';
+import Date from '../components/Date';
 
 // export async function getStaticProps() {
 //   const allPostsData = getSortedPostsData();
